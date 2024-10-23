eb896fe7caf711050d6c71b5b223a26fc7d7d351
Joshua Flowers <flowers.joshuac@gmail.com>
2024-10-22 21:13:37 -0500

    Restructured and reset project directory

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if test ! -e "bin"; then
+	mkdir -p "bin/release"
+	mkdir -p "bin/debug"
+fi
+
+FILES=$(find "src/" -name "*.c")
+LIB_FILES=$(find "lib/" -name "*.o")
+
+COMPILER=clang
+
+if ! command -v clang; then
+	COMPILER=gcc
+fi
+
+BINARY_NAME="chip8"
+
+$COMPILER -xc -std=gnu17 -I "src/headers" -I "lib/headers" -O3 $LIB_FILES $FILES -o "bin/release/$BINARY_NAME"
+$COMPILER -xc -std=gnu17 -I "src/headers" -I "lib/headers" -O0 -g $LIB_FILES $FILES -o "bin/debug/$BINARY_NAME"
